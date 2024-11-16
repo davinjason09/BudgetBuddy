43ae8ed09133d366c45fc6bb27ffb9390847e28d
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-16 14:31:38 +0700

    fix(animation): Fix opacity not being applied properly

--- components/TabContainer.tsx
+++ components/TabContainer.tsx
@@ -17,7 +17,7 @@ const TabContainer = ({ children }: { children: ReactNode }) => {
   const animation = useSharedValue<number>(0);
 
   useEffect(() => {
-    animation.value = withTiming(opened ? 1 : 0, { duration: 300 });
+    animation.value = withTiming(opened ? 1 : 0, { duration: 250 });
   }, [opened]);
 
   const handlePress = () => {
@@ -26,7 +26,7 @@ const TabContainer = ({ children }: { children: ReactNode }) => {
 
   const overlayStyle = useAnimatedStyle(() => {
     return {
-      opacity: interpolate(animation.value, [0, 0.5, 1], [0, 0.25, 0.5]),
+      opacity: interpolate(animation.value, [0, 0.5, 1], [0, 0.2, 0.4]),
     };
   });
 
@@ -35,20 +35,22 @@ const TabContainer = ({ children }: { children: ReactNode }) => {
       {children}
       {opened && (
         <Animated.View
-          style={[styles.overlay, overlayStyle]}
+          style={styles.overlayWrapper}
           entering={FadeIn}
-          exiting={FadeOut.withInitialValues({ opacity: 0.5 })}
+          exiting={FadeOut}
         >
-          <TouchableOpacity
-            style={{ flex: 1 }}
-            activeOpacity={1}
-            onPress={() => handlePress()}
-          >
-            <LinearGradient
+          <Animated.View style={[styles.overlay, overlayStyle]}>
+            <TouchableOpacity
               style={{ flex: 1 }}
-              colors={["transparent", Colors.violet40, Colors.violet60]}
-            />
-          </TouchableOpacity>
+              activeOpacity={1}
+              onPress={() => handlePress()}
+            >
+              <LinearGradient
+                style={{ flex: 1 }}
+                colors={["transparent", Colors.violet40, Colors.violet80]}
+              />
+            </TouchableOpacity>
+          </Animated.View>
         </Animated.View>
       )}
     </View>
@@ -60,13 +62,16 @@ const styles = StyleSheet.create({
     flex: 1,
     zIndex: -1,
   },
-  overlay: {
+  overlayWrapper: {
     position: "absolute",
     top: 0,
     left: 0,
     right: 0,
     bottom: 0,
     zIndex: 1,
+  },
+  overlay: {
+    flex: 1,
   },
 });
 
