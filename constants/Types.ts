09037f3cbae324411c23cfbe6e3262f4ed37002d
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-16 14:29:40 +0700

    feat(types): Removed unused props

--- constants/Types.ts
+++ constants/Types.ts
@@ -1,10 +1,5 @@
 import React from "react";
 
-export type AddButtonProps = {
-  opened: boolean;
-  toggleOpened: () => void;
-};
-
 export type IconProps = {
   colors: string | string[];
   size: number;
