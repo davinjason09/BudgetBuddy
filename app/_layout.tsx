c475aeaad7ff8a92fdf50fbfe75e067252fb9b5e
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-18 01:38:27 +0700

    feat(root): Load fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,75 @@
+import { Colors } from "@/constants/Colors";
 import { FloatingButtons } from "@/constants/FloatingButtons";
+import { LeftArrow } from "@/constants/Icons";
+import { defaultStyles } from "@/constants/Styles";
 import CustomTabBarProvider from "@/context/CustomTabBarContext";
-import TabProvider from "@/context/TabContext";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { useFonts } from "expo-font";
+import { SplashScreen, useRouter } from "expo-router";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-export default function RootLayout() {
+const RootLayout = () => {
+  SplashScreen.preventAutoHideAsync();
+
+  const router = useRouter();
+  const [fontLoaded, fontError] = useFonts({
+    Inter: require("@/assets/fonts/Inter-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontError) {
+      throw fontError;
+    }
+  }, [fontError]);
+
+  useEffect(() => {
+    if (fontLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontLoaded]);
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="actions/[type]"
+        options={{
+          headerTitleAlign: "center",
+          headerShadowVisible: false,
+          headerTitleStyle: defaultStyles.textTitle3,
+          headerTransparent: true,
+          headerLeft: () => (
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={() => router.back()}
+              style={defaultStyles.arrowContainer}
+            >
+              <LeftArrow size={24} colors={Colors.light100} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </Stack>
+  );
+};
+
+const RootLayoutNav = () => {
   return (
     <CustomTabBarProvider
       overlayProps={{ expandingMode: "staging" }}
       data={FloatingButtons}
     >
-      <TabProvider>
-        <Stack>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        </Stack>
-      </TabProvider>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <BottomSheetModalProvider>
+          <RootLayout />
+        </BottomSheetModalProvider>
+      </GestureHandlerRootView>
     </CustomTabBarProvider>
   );
-}
+};
+
+export default RootLayoutNav;
