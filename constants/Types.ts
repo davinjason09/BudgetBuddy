2ba6c19e15fb730a28cbaf79be3222c6a7abc182
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-19 13:00:46 +0700

    feat(types): Added types for Button, Picker, Overview, Attachment, and Switch

--- constants/Types.ts
+++ constants/Types.ts
@@ -1,8 +1,35 @@
 import React from "react";
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
 
 export type IconProps = {
   colors: string | string[];
   size: number;
+};
+
+export type ButtonProps = {
+  onPress?: () => void;
+  onLongPress?: () => void;
+  title: string;
+  iconLeft?: React.ReactNode;
+  iconRight?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
+  disabled: boolean;
+  activeOpacity?: number;
+  loading: boolean;
+};
+
+export type PickerProps = {
+  value: any;
+  data: any[];
+  isEditable?: boolean;
+  placeholder?: string;
+  labelField: string;
+  valueField: string;
+  style?: StyleProp<ViewStyle>;
+  iconRight?: React.JSX.Element;
+  iconLeft?: React.JSX.Element;
+  onChange: (item: any) => void;
 };
 
 export type CustomTabBarPassThroughParams = {
@@ -24,4 +51,22 @@ export type CustomTabBarContextProps = {
   opened: boolean;
   overlayProps?: CustomTabBarOverlayProps;
   toggleOpened: () => void;
+};
+
+export type OverviewProps = {
+  type: "income" | "expense";
+  amount: number;
+  overviewStyle?: StyleProp<ViewStyle>;
+};
+
+export type AttachmentProps = {
+  data: string;
+  setData: (data: string) => void;
+};
+
+export type SwitchProps = {
+  value: boolean;
+  setValue: (value: boolean) => void;
+  activeColor?: string;
+  inactiveColor?: string;
 };
