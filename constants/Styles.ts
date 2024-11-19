5274081daa02f63efd64bb4d8ea1132079f266eb
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-19 13:01:48 +0700

    feat(styles): Added default styles for page, back arrow, and button

--- constants/Styles.ts
+++ constants/Styles.ts
@@ -1,49 +1,78 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "./Colors";
 
 export const defaultStyles = StyleSheet.create({
+  // Page Styles
+  pageContainer: {
+    flex: 1,
+    backgroundColor: Colors.light100,
+  },
+
+  // Back arrow
+  arrowContainer: {
+    width: 30,
+    height: 30,
+    marginLeft: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  // Button
+  button: {
+    width: "85%",
+    backgroundColor: Colors.violet100,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    gap: 10,
+    borderRadius: 16,
+    height: 56,
+  },
+
+  // Text Styles
   textTitleX: {
     fontSize: 64,
-    fontWeight: 700,
+    fontWeight: "700",
     fontFamily: "Inter",
   },
   textTitle1: {
     fontSize: 32,
-    fontWeight: 700,
+    fontWeight: "700",
     fontFamily: "Inter",
   },
   textTitle2: {
     fontSize: 24,
-    fontWeight: 600,
+    fontWeight: "600",
     fontFamily: "Inter",
   },
   textTitle3: {
     fontSize: 18,
-    fontWeight: 600,
+    fontWeight: "600",
     fontFamily: "Inter",
   },
   textRegular1: {
     fontSize: 16,
-    fontWeight: 500,
+    fontWeight: "500",
     fontFamily: "Inter",
   },
   textRegular2: {
     fontSize: 16,
-    fontWeight: 600,
+    fontWeight: "600",
     fontFamily: "Inter",
   },
   textRegular3: {
     fontSize: 14,
-    fontWeight: 400,
+    fontWeight: "400",
     fontFamily: "Inter",
   },
   textSmall: {
     fontSize: 23,
-    fontWeight: 500,
+    fontWeight: "500",
     fontFamily: "Inter",
   },
   textTiny: {
     fontSize: 12,
-    fontWeight: 500,
+    fontWeight: "500",
     fontFamily: "Inter",
   },
 });
