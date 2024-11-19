197ea645006dc4490ca7b2a75da2e11d3bcce596
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-19 13:02:07 +0700

    feat(options): Data for the dropdown picker

---
+++ constants/Options.tsx
@@ -0,0 +1,19 @@
+export const banks = [
+  { label: "Chase Bank", value: "chase" },
+  { label: "Paypal", value: "paypal" },
+  { label: "Citi Bank", value: "citi" },
+  { label: "Bank of America", value: "america" },
+  { label: "Jago", value: "jago" },
+  { label: "Mandiri", value: "mandiri" },
+  { label: "BCA", value: "bca" },
+  { label: "Other", value: "other" },
+];
+
+export const categories = [
+  { label: "Food", value: "food" },
+  { label: "Transportation", value: "transportation" },
+  { label: "Shopping", value: "shopping" },
+  { label: "Subscription", value: "subscription" },
+  { label: "Salary", value: "salary" },
+  { label: "Other", value: "other" },
+];
