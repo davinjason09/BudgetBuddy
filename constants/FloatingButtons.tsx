759dfe5aedc3f587310643d8df95e0d12fbe5764
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-23 13:04:22 +0700

    feat(floating-button): use expo's routing instead of react navigation
    - React Navigation can't handle grouped routes like expo router

--- constants/FloatingButtons.tsx
+++ constants/FloatingButtons.tsx
@@ -1,40 +1,57 @@
 import { Colors } from "./Colors";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import { Income, Expense, Transfer } from "./Icons";
+import { useRouter } from "expo-router";
 
 export const FloatingButtons = [
-  ({ params }: { params?: any }) => {
+  () => {
+    const router = useRouter();
     return (
       <TouchableOpacity
         style={[styles.item, { backgroundColor: Colors.green100 }]}
         onPress={() => {
-          params.push("actions/[type]", { type: "income" });
+          router.push({
+            pathname: "/(actions)/[type]",
+            params: { type: "income" },
+          });
         }}
       >
         <Income colors={Colors.light100} size={24} />
       </TouchableOpacity>
     );
   },
-  ({ params }: { params?: any }) => (
-    <TouchableOpacity
-      style={[styles.item, { backgroundColor: Colors.blue100 }]}
-      onPress={() => {
-        params.navigate("actions/[type]", { type: "transfer" });
-      }}
-    >
-      <Transfer colors={Colors.light100} size={24} />
-    </TouchableOpacity>
-  ),
-  ({ params }: { params?: any }) => (
-    <TouchableOpacity
-      style={[styles.item, { backgroundColor: Colors.red100 }]}
-      onPress={() => {
-        params.navigate("actions/[type]", { type: "expense" });
-      }}
-    >
-      <Expense colors={Colors.light100} size={24} />
-    </TouchableOpacity>
-  ),
+  () => {
+    const router = useRouter();
+    return (
+      <TouchableOpacity
+        style={[styles.item, { backgroundColor: Colors.blue100 }]}
+        onPress={() => {
+          router.push({
+            pathname: "/(actions)/[type]",
+            params: { type: "transfer" },
+          });
+        }}
+      >
+        <Transfer colors={Colors.light100} size={24} />
+      </TouchableOpacity>
+    );
+  },
+  () => {
+    const router = useRouter();
+    return (
+      <TouchableOpacity
+        style={[styles.item, { backgroundColor: Colors.red100 }]}
+        onPress={() => {
+          router.push({
+            pathname: "/(actions)/[type]",
+            params: { type: "expense" },
+          });
+        }}
+      >
+        <Expense colors={Colors.light100} size={24} />
+      </TouchableOpacity>
+    );
+  },
 ];
 
 const styles = StyleSheet.create({
