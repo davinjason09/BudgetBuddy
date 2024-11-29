49c278e9e3c4704f5eb1a639a2ec49fd256983d2
davinjason09 <davinjasonevanraharjo@mail.ugm.ac.id>
2024-11-29 09:48:08 +0700

    feat(Types): Add and edit some types
    - New types for Transaction, Wallet, and Budget details

--- constants/Types.ts
+++ constants/Types.ts
@@ -1,5 +1,33 @@
 import { StyleProp, TextStyle, ViewStyle } from "react-native";
 
+export type TransactionDetails = {
+  id: number;
+  amount: number;
+  date: string;
+  type: string;
+  category: string;
+  wallet_id: number;
+  description: string;
+  created_at: string;
+};
+
+export type WalletInfo = {
+  id: number;
+  name: string;
+  balance: number;
+  type: string;
+};
+
+export type BudgetInfo = {
+  id: number;
+  amount: number;
+  month: number;
+  year: number;
+  category: string;
+  alert: number;
+  percentage: number;
+};
+
 export type IconProps = {
   colors?: string | string[];
   size: number;
@@ -33,11 +61,12 @@ export type PickerProps = {
 
 export type ChipProps = {
   variant: "default" | "outlined";
-  query: string;
+  query: any;
   text?: string;
   icon?: React.ReactNode;
-  value: string;
-  onPress: (value: string) => void;
+  value: any;
+  onPress: (value: any) => void;
+  editable: boolean;
   style?: StyleProp<ViewStyle>;
   textStyle?: StyleProp<TextStyle>;
   selectedStyle?: StyleProp<ViewStyle>;
@@ -55,6 +84,18 @@ export type MonthYearSelectorProps = {
   textStyle?: StyleProp<TextStyle>;
   arrowColor?: string;
   arrowType: "arrow" | "chevron";
+};
+
+export type TransactionInfoProps = {
+  data: TransactionDetails;
+};
+
+export type WalletInfoProps = {
+  data: WalletInfo;
+};
+
+export type BudgetInfoProps = {
+  data: BudgetInfo;
 };
 
 export type CustomTabBarOverlayProps = {
